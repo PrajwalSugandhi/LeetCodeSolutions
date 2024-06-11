b9327411554afe14e240d5677f2bf1ae4bb6b809
Prajwal Sugandhi <106886624+PrajwalSugandhi@users.noreply.github.com>
2024-06-11 17:54:03 +0530

    Time: 4 ms (31.60%) | Memory: 9.2 MB (34.97%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -23,8 +23,24 @@ public:
 
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size(),-1);
-        int ans;
-        ans = best(nums, dp, nums.size()-1);
-        return ans;
+        // int ans;
+        // ans = best(nums, dp, nums.size()-1);
+        int pick,nopick;
+        dp[0] = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            if(i > 1){
+                pick = nums[i] + dp[i-2];
+            }
+            else{
+                pick = nums[i];
+            }
+            
+            nopick = 0 + dp[i-1];
+
+            dp[i] = max(pick,nopick);
+        }
+
+
+        return dp[nums.size()-1];
     }
 };
