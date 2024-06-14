fb988b2f666beb38f3973cd80e9e0ef0c6224f44
Prajwal Sugandhi <106886624+PrajwalSugandhi@users.noreply.github.com>
2024-06-14 21:09:31 +0530

    Time: 335 ms (15.62%) | Memory: 16.3 MB (60.81%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -49,19 +49,44 @@ public:
             return false;
         }
         target /= 2;
-        vector<vector<int>> dp(nums.size(), vector<int>(target+1, -1));
+        // vector<vector<int>> dp(nums.size(), vector<int>(target+1, -1));
         int n = nums.size();
         // cout<<target<<" ";
-        int ans = recur(nums, n-1,target, dp);
+        // int ans = recur(nums, n-1,target, dp);
         // for(auto i : dp){
         //     for(int j : i){
         //         cout<<j<<" ";
         //     }
         //     cout<<endl;
         // }
-        if(ans == 1){
-            return true;
+        // if(ans == 1){
+        //     return true;
+        // }
+        // return false;
+        vector<vector<bool>> dp(nums.size(), vector<bool>(target+1, -1));
+        for(int i = 0; i<n; i++){
+            dp[i][0] = true;
         }
-        return false;
+        bool pick=0, nopick=0;
+        for(int left = 1; left<= target; left++){
+            if(left == nums[0]){
+                dp[0][left] = true;
+            }
+            else{
+                dp[0][left] = false;
+            }
+        }
+        for(int i = 1; i<n; i++){
+            for(int left = 1; left<=target; left++){
+                pick = 0;
+                nopick = dp[i-1][left];
+                if(left >= nums[i]){
+                    pick = dp[i-1][left - nums[i]];
+                }
+                dp[i][left] = nopick | pick;
+            }
+        }
+        return dp[n-1][target];
+
     }
 };
